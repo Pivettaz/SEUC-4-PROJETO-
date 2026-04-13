4c65bd8519785d08c5f501e9a44984b979b0703b
Leonardo Varela Vacari <leonardo.vv1@puccampinas.edu.br>
2026-04-13 12:46:35 -0300

    Reajuste nos prints do código

--- funcao_ajuste.py
+++ funcao_ajuste.py
@@ -1,14 +1,18 @@
 def ajuste_pressao ():
-    num_pressoes=int(input('qtnd pressões: '))
+    num_pressoes=int(input('Quantidade de pressões: '))
     cont_pressoes=0
     soma_pressoes_ajustadas=0
     for i in range (0, num_pressoes):
-        pressao=int(input('digite a pressão em UCPs: '))
+        pressao=int(input('Digite a pressão em UCPs: '))
         cont_pressoes+=1
         if (pressao > 150):
             pressao_ajustada = pressao * 1.08
         else: 
             pressao_ajustada = pressao * 0.96
         soma_pressoes_ajustadas += pressao_ajustada
-        print(f'\n qtnd de pressões: {cont_pressoes} \n pressão ajustada: {pressao_ajustada} \n soma das pressões: {soma_pressoes_ajustadas}')
+        print(f"Pressão ajustada: {pressao_ajustada}")
 
+    print(f"Quantidade de pressões: {cont_pressoes}")
+    print(f"Soma das pressões: {soma_pressoes_ajustadas}")
+
+# Adicionar a menor pressão registrada
