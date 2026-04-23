d644ccb89e972e444d1c2675e4eaf7c256afb4de
Vinicius Hegues Baiocco <vinicius.baiocco@gmail.com>
2026-04-23 12:44:07 -0300

    Versão final do ajuste_pressao, integração com o funcao_estabilidade

--- funcao_ajuste.py
+++ funcao_ajuste.py
@@ -1,5 +1,4 @@
-# NEXT: descomentar e ajustar o import abaixo quando o arquivo de classificação ficar pronto
-# from Classificacao_Estabilidade import classificacao_estabilidade (arquivo do rafa)
+from funcao_estabilidade import classificacao_estabilidade
 
 def ler_quantidade():
     return int(input("Quantidade de pressões: "))
@@ -11,8 +10,13 @@ def ler_pressao():
 def ajuste_pressao():
     quantidade = ler_quantidade()
     soma = 0
-    menor_pressao = 99999 ## se conseguir otimizar essa parte da menor pressao, nao consegui pensar a nao ser em criar uma execucao fora com menor_pressao = pressao_ajustada
+    menor_pressao = 99999
+    zona_verde = 0
+    leituras_realizadas = 0
+    zona_anterior = ""
+    houve_travamento = 0 # se caso for 1, printar a qtde de leituras realizadas, oque não é pedido caso nao ocorra travamento
     for _ in range(quantidade):
+        leituras_realizadas += 1
         pressao = ler_pressao()
         if pressao > 150:
             pressao_ajustada = pressao * 1.08
@@ -22,12 +26,15 @@ def ajuste_pressao():
         if pressao_ajustada < menor_pressao:
             menor_pressao = pressao_ajustada
         print(f"Pressão ajustada: {pressao_ajustada:.2f}UCPs")
-
-        # NEXT: chamar classificacao_estabilidade(pressao_ajustada) e guardar zona
-        # NEXT: contar zona_verde_cont e detectar travamento
-        # NEXT: adicionar break caso zona vermelha + de 2 vezes
-        # NEXT: media das pressoes 
-    return quantidade, soma, menor_pressao
+        zona = classificacao_estabilidade(pressao_ajustada)
+        if zona == "Verde":
+            zona_verde += 1
+        if zona == "Vermelha" and zona_anterior == "Vermelha":
+            houve_travamento = 1
+            print("Protocolo de Travamento: duas leituras seguidas de pressões na zona vermelha")
+            break
+        zona_anterior = zona
+    return quantidade, soma, menor_pressao, zona_verde, leituras_realizadas, houve_travamento
 
 # NEXT EXTRA: maior pressão ajustada, acho um extra legal para colocar
 
