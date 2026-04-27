f02fa3a93d85a8d380d4df4027d4e4e09c008a2a
Vinicius Hegues Baiocco <vinicius.baiocco@gmail.com>
2026-04-27 10:50:36 -0300

    correção de calculos internos, solicitado pela professora, tambem feat nova de mostrar a zona que está em cada leitura

--- funcao_ajuste.py
+++ funcao_ajuste.py
@@ -4,17 +4,16 @@ def ler_quantidade():
     return int(input("Quantidade de pressões: "))
 
 def ler_pressao():
-    return int(input("Digite a pressão em UCPs: "))
+    return float(input("Digite a pressão em UCPs: "))
 
-## dividi em 3 funcoes para ficar mais limpo o codigo, as funcoes de ler sao chamadas na funcao ajuste (principal)
 def ajuste_pressao():
     quantidade = ler_quantidade()
     soma = 0
-    menor_pressao = 99999
+    menor_pressao = float("inf")
     zona_verde = 0
     leituras_realizadas = 0
     zona_anterior = ""
-    houve_travamento = 0 # se caso for 1, printar a qtde de leituras realizadas, oque não é pedido caso nao ocorra travamento
+    houve_travamento = 0
     for _ in range(quantidade):
         leituras_realizadas += 1
         pressao = ler_pressao()
@@ -25,8 +24,8 @@ def ajuste_pressao():
         soma += pressao_ajustada
         if pressao_ajustada < menor_pressao:
             menor_pressao = pressao_ajustada
-        print(f"Pressão ajustada: {pressao_ajustada:.2f}UCPs")
         zona = classificacao_estabilidade(pressao_ajustada)
+        print(f"Pressão ajustada: {pressao_ajustada:.2f} UCPs | Zona: {zona}")
         if zona == "Verde":
             zona_verde += 1
         if zona == "Vermelha" and zona_anterior == "Vermelha":
@@ -34,10 +33,8 @@ def ajuste_pressao():
             print("Protocolo de Travamento: duas leituras seguidas de pressões na zona vermelha")
             break
         zona_anterior = zona
-    media = soma/leituras_realizadas
-    porcentagem_verde = zona_verde*100 / leituras_realizadas
-    return quantidade, soma, menor_pressao, zona_verde, leituras_realizadas, houve_travamento, media, porcentagem_verde
+    media = soma / leituras_realizadas
+    porcentagem_verde = zona_verde * 100 / leituras_realizadas
+    percentual_leituras = (leituras_realizadas / quantidade) * 100
+    return menor_pressao, media, porcentagem_verde, percentual_leituras, houve_travamento
 
-# NEXT EXTRA: maior pressão ajustada, acho um extra legal para colocar
-
-ajuste_pressao() ## remover, sera chamada pelo menu.py
