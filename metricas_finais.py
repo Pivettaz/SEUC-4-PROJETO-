4fb5a237fff0b4b03e7b90b63d874f7babd77ea2
Vinicius Hegues Baiocco <vinicius.baiocco@gmail.com>
2026-04-27 10:08:47 -0300

    feat: metricas finais, mostrando tudo que é pedido pelo programa

--- metricas_finais.py
+++ metricas_finais.py
@@ -1,2 +1,13 @@
-from funcao_ajuste import funcao_ajuste
+from funcao_ajuste import ajuste_pressao
+
 def metricas_finais():
+    quantidade, soma, menor_pressao, zona_verde, leituras_realizadas, houve_travamento, media, porcentagem_verde, percentual_leituras = ajuste_pressao()
+    print("")
+    print("\n=== METRICAS FINAIS ===")
+    print("Média das pressões ajustadas: {:.2f}".format(media))
+    print("Menor pressão ajustada: {:.2f}". format(menor_pressao))
+    print("Porcentagem de leituras que ficaram na zona verde: {:.2f}".format(porcentagem_verde))
+    print("Percentual de leituras realizadas: {:.2f}".format(percentual_leituras))
+    print("==========================================================")
+
+metricas_finais()
